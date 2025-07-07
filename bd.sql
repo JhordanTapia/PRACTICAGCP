6a5a66f2fb6fd07d53480a40a5f444815a98b9db
JhordanTapia <jhordantapiaespinoza@gmail.com>
2025-07-07 11:01:09 -0500

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -1,23 +1,22 @@
-
 CREATE TABLE Personas_InfoAdicional (
-  persona_id INT64 NOT NULL,
-  origen STRING(50),
-  actividad STRING(100),
-  distrito STRING(50),
-  institucion STRING(100),
-  estado STRING(20)
-) PRIMARY KEY(persona_id);
+  persona_id BIGINT NOT NULL PRIMARY KEY,
+  origen VARCHAR(50),
+  actividad VARCHAR(100),
+  distrito VARCHAR(50),
+  institucion VARCHAR(100),
+  estado VARCHAR(20)
+);
 
 CREATE TABLE Personas (
-  persona_id INT64 NOT NULL,
-  nombres STRING(50),
-  apellidos STRING(50),
+  persona_id BIGINT NOT NULL PRIMARY KEY,
+  nombres VARCHAR(50),
+  apellidos VARCHAR(50),
   fecha_nacimiento DATE,
-  telefonos ARRAY<STRING(15)>,  -- atributo multivaluado
-  correo STRING(100)
-) PRIMARY KEY(persona_id),
-  INTERLEAVE IN PARENT Personas_InfoAdicional ON DELETE CASCADE;
-
+  telefono1 VARCHAR(15),
+  telefono2 VARCHAR(15),
+  correo VARCHAR(100),
+  FOREIGN KEY (persona_id) REFERENCES Personas_InfoAdicional(persona_id)
+);
 
 INSERT INTO Personas_InfoAdicional (persona_id, origen, actividad, distrito, institucion, estado) VALUES
 (1, 'Chimbote', 'Estudia Ingeniería', 'Nuevo Chimbote', 'UNS', 'Activo'),
@@ -26,13 +25,13 @@ INSERT INTO Personas_InfoAdicional (persona_id, origen, actividad, distrito, ins
 (4, 'Trujillo', 'Docente universitario', 'La Esperanza', 'UNT', 'Activo'),
 (5, 'Arequipa', 'Estudia Derecho', 'Cercado', 'UCSP', 'Activo');
 
-
 INSERT INTO Personas (
-  persona_id, nombres, apellidos, fecha_nacimiento, telefonos, correo
+  persona_id, nombres, apellidos, fecha_nacimiento, telefono1, telefono2, correo
 ) VALUES
-(1, 'Jhordan', 'Tapia', DATE '2001-04-03', ['937507475','948138671'], 'jhordantapiaespinoza@gmail.com'),
-(2, 'Rocio', 'Espinoza', DATE '1998-11-20', ['948138671', '934567812'], 'rocioea_11@hotmail.com'),
-(3, 'Harold', 'Tapia', DATE '1993-05-30', ['978123456'], 'haroldtapiaespinoza@gmail.com'),
-(4, 'Lucía', 'Fernández', DATE '1997-07-15', ['912345678'], 'luciaf@gmail.com'),
-(5, 'José', 'Vega', DATE '1999-03-22', ['987654321'], 'jose.v@gmail.com');
+(1, 'Jhordan', 'Tapia', '2001-04-03', '937507475', '948138671', 'jhordantapiaespinoza@gmail.com'),
+(2, 'Rocio', 'Espinoza', '1998-11-20', '948138671', '934567812', 'rocioea_11@hotmail.com'),
+(3, 'Harold', 'Tapia', '1993-05-30', '978123456', NULL, 'haroldtapiaespinoza@gmail.com'),
+(4, 'Lucía', 'Fernández', '1997-07-15', '912345678', NULL, 'luciaf@gmail.com'),
+(5, 'José', 'Vega', '1999-03-22', '987654321', NULL, 'jose.v@gmail.com');
+
 
